6492bf2e6e2eb4ca4884c69cd4d08e01c84d1246
Callum Osborn <callumosborn@hotmail.com>
2024-03-12 20:25:51 +0000

    add merge sort bottom-up approach

--- internal/algorithms/sort/merge_sort.go
+++ internal/algorithms/sort/merge_sort.go
@@ -50,3 +50,56 @@ func (s MergeSort) sort(A *[]int) {
 
 	rsort(0, len(*A)-1)
 }
+
+// Non-Recursive, Bottom-Up Approach
+func (s MergeSort) sort2(A *[]int) {
+	n := len(*A)
+	aux := make([]int, n)
+
+	var (
+		min   func(a int, b int) int
+		merge func(lo int, mid int, hi int)
+	)
+
+	min = func(a int, b int) int {
+		if a < b {
+			return a
+		}
+
+		return b
+	}
+
+	merge = func(lo int, mid int, hi int) {
+		copy(aux[lo:hi+1], (*A)[lo:hi+1])
+
+		left := lo
+		right := mid + 1
+
+		for i := lo; i < hi+1; i++ {
+
+			if left > mid {
+				(*A)[i] = aux[right]
+				right += 1
+			} else if right > hi {
+				(*A)[i] = aux[left]
+				left += 1
+			} else if aux[right] < aux[left] {
+				(*A)[i] = aux[right]
+				right += 1
+			} else {
+				(*A)[i] = aux[left]
+				left += 1
+			}
+		}
+	}
+
+	for i := 1; i <= n-1; i = i + i {
+
+		for lo := 0; lo < n-i; lo += i + i {
+			mid := min(lo+i-1, n-1)
+			hi := min(lo+2*i-1, n-1)
+
+			merge(lo, mid, hi)
+		}
+	}
+}
--- internal/algorithms/sort/merge_sort_test.go
+++ internal/algorithms/sort/merge_sort_test.go
@@ -46,3 +46,45 @@ func TestMergeSort(t *testing.T) {
 		})
 	}
 }
+
+func TestMergeSort2(t *testing.T) {
+	tests := []struct {
+		name     string
+		A        []int
+		expected []int
+	}{
+		{
+			name:     "Empty Slice",
+			A:        []int{},
+			expected: []int{},
+		},
+		{
+			name:     "Random Order",
+			A:        []int{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5},
+			expected: []int{1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9},
+		},
+		{
+			name:     "Decreasing Order",
+			A:        []int{9, 8, 7, 6, 5, 4, 3, 2, 1},
+			expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
+		{
+			name:     "Sorted Order",
+			A:        []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+			expected: []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
+	}
+
+	s := MergeSort{}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			A := tt.A
+			s.sort2(&A)
+
+			if !reflect.DeepEqual(A, tt.expected) {
+				t.Errorf("sort() = %v, expected %v", A, tt.expected)
+			}
+		})
+	}
+}
